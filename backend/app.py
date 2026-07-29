3377c8ffb164ee7e525a94aa0d2d2cbd9ba5e336
I-abMoeez <imoeezqureshi@gmail.com>
2026-07-29 15:51:51 +0500

    changes for deploy

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,15 @@
 from flask import Flask
 from flask_cors import CORS
-from routes.dashboard import dashboard_bp
-from routes.predict import predict_bp
+import os
 
 app = Flask(__name__)
-CORS(app)  # allows React frontend to call this API
+CORS(app)
 
-app.register_blueprint(dashboard_bp, url_prefix="/api/dashboard")
-app.register_blueprint(predict_bp,   url_prefix="/api/predict")
+@app.route("/")
+def home():
+    return "Backend is running!"
 
+# IMPORTANT: for Render
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
